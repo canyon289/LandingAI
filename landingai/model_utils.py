014b629400b682509504bfaff96ae41a79f6a3e0
Ravin Kumar <ravinsdrive@gmail.com>
2017-12-23 12:56:57 -0800

    Add model utility module

---
+++ landingai/model_utils.py
@@ -0,0 +1,39 @@
+"""
+Utility functions to be used when training the model
+"""
+from keras.preprocessing import image
+import numpy as np
+import os
+
+
+def load_image(img_filename):
+    """Takes an image and loads it as a tensor"""
+    img = image.load_img(img_filename)
+    x = image.img_to_array(img)
+    return np.expand_dims(x, axis=0)
+
+
+def class_label(img_filename):
+    """Given an image path return class label"""
+    return int(img_filename[6])
+
+
+def load_data(path):
+    """Takes path and returns tensor and class labels
+
+    Parameters
+    ----------
+    path: str
+        Name of path that contains images
+
+    Returns
+    -------
+        array: numpy array
+            Tensors that reprsent input image
+        targets :
+            Two column vector for class prediction
+    """
+    img_files = os.listdir(path)
+    imgs = [load_image(os.path.join(path, img)) for img in img_files]
+    targets = np.array([class_label(img) for img in img_files])
+    return imgs, targets
